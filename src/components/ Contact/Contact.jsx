a3b56b1a7839065eb1a1c309dd609829a6866ef1
Jeffrey Sojack <jeffrey.sojack@gmail.com>
2020-02-25 21:33:27 -0500

    added contact form

--- src/components/ Contact/Contact.jsx
+++ src/components/ Contact/Contact.jsx
@@ -1,17 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './contact.scss'
 
 const Contact = () => {
-	return (
-		<section className="contact" id="contact">
-			<h1 className="contactTitle">Contact Us</h1>
-			<p className="contactBody">
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis, illo sapiente. Tenetur obcaecati
-				quidem, qui alias quam quibusdam assumenda neque dolore, fugit accusamus illum voluptatem soluta eaque
-				corporis voluptates iste.
-			</p>
-		</section>
-	)
+  const [contactForm, setContactForm] = useState({
+    id: 0,
+    name: '',
+    email: '',
+    message: '',
+  })
+
+  const updateContactObject = e => {
+    e.persist()
+    setContactForm(prevContactForm => ({
+      ...prevContactForm,
+      [e.target.name]: e.target.value,
+    }))
+  }
+  return (
+    <section className="contact" id="contact">
+      <h1 className="contactTitle">Contact Us</h1>
+      <section className="contactForm">
+        <section className="inner">
+          <img src="/Assets/airMail.jpg" className="stamp" />
+          <form>
+            <input
+              className="formName"
+              name="name"
+              type="text"
+              value={contactForm.name}
+              placeholder="Enter Your Name"
+              onChange={updateContactObject}
+            />
+            <input
+              className="formEmail"
+              name="email"
+              type="email"
+              value={contactForm.email}
+              placeholder="Enter Your Email"
+              onChange={updateContactObject}
+            />
+            <textarea
+              className="formMessage"
+              name="message"
+              type="string"
+              placeholder="Enter Message"
+              row="10"
+              onChange={updateContactObject}
+            >
+              {contactForm.message}
+            </textarea>
+          </form>
+          <button className="submitButton">Submit</button>
+        </section>
+      </section>
+    </section>
+  )
 }
 
 export default Contact
